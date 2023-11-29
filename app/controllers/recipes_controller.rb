61b99e776890a92faf7d3cec1246534452e90bc7
prg-04 <karanjaevans95@gmail.com>
2023-11-29 13:59:10 +0300

    Fix linter error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,8 +24,7 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /recipes or /recipes.json
   def create
@@ -33,7 +32,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -45,14 +44,14 @@ class RecipesController < ApplicationController
   # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
-    if @recipe.update(recipe_params)
-      format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
-      format.json { render :show, status: :ok, location: @recipe }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      if @recipe.update(recipe_params)
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
     end
-  end
   end
 
   # DELETE /recipes/1 or /recipes/1.json
@@ -60,25 +59,30 @@ class RecipesController < ApplicationController
     @recipe.destroy!
 
     respond_to do |format|
-      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-      @recipe.user = current_user
-    end
 
-    # Only allow a list of trusted parameters through.
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public).merge(user_id: current_user.id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+    @recipe.user = current_user
+  end
 
-    # Additional check to ensure only the owner can delete a recipe
-    def authorize_owner!
-      redirect_to recipes_path, alert: "You don't have permission to delete this recipe." unless @recipe.user == current_user
-    end
+  # Only allow a list of trusted parameters through.
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description,
+                                   :public).merge(user_id: current_user.id)
+  end
+
+  # Additional check to ensure only the owner can delete a recipe
+  def authorize_owner!
+    return if @recipe.user == current_user
+
+    redirect_to recipes_path,
+                alert: "You don't have permission to delete this recipe."
+  end
 end
