2e7455e4a1c815dd10b62b40ea07e036e8a2d962
ishimwepati <wazacode@gmail.com>
2023-11-30 14:13:18 +0300

    Initialise

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :recipes
-  resources :private_recipes, only: %i[index show]
-  devise_for :users, controllers: {
-    sessions: "user/sessions",
-    registrations: "user/registrations",
-  }
-  root "main#index"
-end
